5e546b8862ed97577cd60f5d7294d2b13bdb950c
Kelly Maass <kmaass77627@gmail.com>
2025-11-21 13:43:05 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 const init = () => {
-    
-    fetch("http://localhost:3003/movies")
-    .then((response) => response.json())
-    .then((json) => findMovie(json))
+    document.querySelector('form').addEventListener('submit', (e) => handleSubmit(e));
 
-    function findMovie(movies) {
-    console.log(movies)
-    document.querySelector('form').addEventListener('submit', (e) => {
-        e.preventDefault()
-        console.log(e.target.children[1].value)
-        let id = e.target.children[1].value
-        console.log(id)
-        let movie = movies.find((movie) => movie.id == id)
-        console.log(movie)
-        let title = document.querySelector('h4')
-        title.innerHTML = movie.title
-        let summary = document.querySelector('p')
-        summary.innerHTML = movie.summary
+    function handleSubmit(e) {
+        e.preventDefault();
+        const movieId = e.target.children[1].value;
+        
+        fetch(`http://localhost:3000/movies/${movieId}`)
+            .then((response) => response.json())
+            .then((movie) => displayMovie(movie));
 
-    })
-    
     }
-   
 
+    function displayMovie(movie) {
+        console.log(movie);
+        const title = document.querySelector('h4');
+        title.innerHTML = movie.title;
+        const summary = document.querySelector('p');
+        summary.innerHTML = movie.summary;
+
+    }
     
-  
-}
-
+};
 document.addEventListener('DOMContentLoaded', init);
